0573eaccc3311d72f13f8496d13231eea155ecf0
miloradovic <miloradovic@outlook.com>
2026-02-11 00:14:55 +0100

    refactor: remove BetPlaced event and LogBetActivity listener from the application

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Events\BetPlaced;
-use App\Listeners\LogBetActivity;
 use App\Models\Transaction;
 use App\Observers\TransactionObserver;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -28,10 +25,5 @@ class AppServiceProvider extends ServiceProvider
         Vite::prefetch(concurrency: 3);
 
         Transaction::observe(TransactionObserver::class);
-
-        Event::listen(
-            BetPlaced::class,
-            LogBetActivity::class,
-        );
     }
 }
